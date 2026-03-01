e55d197acb859999b896022be0f22eee92a7e0eb
PopsicleYT <gerbitravis@gmail.com>
2026-03-01 14:01:58 -0700

    Update FontPicker.hpp

--- src/nodes/FontPicker.hpp
+++ src/nodes/FontPicker.hpp
@@ -1,11 +1,17 @@
 #pragma once
+#include <Geode/Geode.hpp>
+#include <Geode/ui/Popup.hpp>
+
+using namespace geode::prelude;
 
 class FontPickerPopup : public geode::Popup<std::string const&, std::function<void(std::string const&)> const&> {
 protected:
     bool setup(std::string const& font, std::function<void(std::string const&)> const& callback) override;
-    void nextFont(CCObject*);
-    void prevFont(CCObject*);
-    void updateFont() const;
+    
+    // Changed CCObject to cocos2d::CCObject for safety
+    void nextFont(cocos2d::CCObject* sender);
+    void prevFont(cocos2d::CCObject* sender);
+    void updateFont(); // Removed 'const' to match .cpp
 
 public:
     static FontPickerPopup* create(std::string const& font, std::function<void(std::string const&)> const& callback);
@@ -13,23 +19,23 @@ public:
     static std::string lookupFont(std::string const& fontFile);
 
 private:
-    size_t m_fontIndex{};
-    cocos2d::CCLabelBMFont* m_fontLabel{};
-    CCMenuItemSpriteExtra* m_nextFontButton{};
-    CCMenuItemSpriteExtra* m_prevFontButton{};
+    size_t m_fontIndex = 0;
+    cocos2d::CCLabelBMFont* m_fontLabel = nullptr;
+    CCMenuItemSpriteExtra* m_nextFontButton = nullptr;
+    CCMenuItemSpriteExtra* m_prevFontButton = nullptr;
 };
 
 class FontPicker : public CCMenuItemSpriteExtra {
 protected:
     bool init(std::string const& font, std::function<void(std::string const&)> const& callback);
-    void onPickFont(CCObject*);
+    void onPickFont(cocos2d::CCObject* sender);
 
 public:
     static FontPicker* create(std::string const& font, std::function<void(std::string const&)> const& callback);
     void setFont(std::string const& font);
 
 protected:
-    cocos2d::CCLabelBMFont* m_fontLabel{};
-    std::string m_font{};
-    std::function<void(std::string const&)> m_callback{};
+    cocos2d::CCLabelBMFont* m_fontLabel = nullptr;
+    std::string m_font;
+    std::function<void(std::string const&)> m_callback;
 };
