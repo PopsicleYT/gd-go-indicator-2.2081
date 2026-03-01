504105c5f69f505d0848de465ebd97867bfbe924
PopsicleYT <gerbitravis@gmail.com>
2026-03-01 13:58:14 -0700

    Update ColorPicker.hpp

--- src/nodes/ColorPicker.hpp
+++ src/nodes/ColorPicker.hpp
@@ -1,16 +1,25 @@
 #pragma once
+#include <Geode/Geode.hpp>
 
-class ColorPicker : public CCMenuItemSpriteExtra, public geode::ColorPickPopupDelegate {
+using namespace geode::prelude;
+
+// Remove geode::ColorPickPopupDelegate as it no longer exists in v5
+class ColorPicker : public CCMenuItemSpriteExtra {
 protected:
+    // Standard init
     bool init(cocos2d::ccColor4B const& value, std::function<void(cocos2d::ccColor4B const&)> const& callback);
-    void updateColor(cocos2d::ccColor4B const& color) override;
-    void onPickColor(CCObject*);
+    
+    // updateColor is no longer an 'override' because the delegate is gone
+    void updateColor(cocos2d::ccColor4B const& color);
+    
+    // This handles the button click to open the popup
+    void onPickColor(cocos2d::CCObject* sender);
 
 public:
     static ColorPicker* create(cocos2d::ccColor4B const& value, std::function<void(cocos2d::ccColor4B const&)> const& callback);
 
 protected:
     std::function<void(cocos2d::ccColor4B const&)> m_callback;
-    cocos2d::CCSprite* m_colorSprite{};
+    cocos2d::CCSprite* m_colorSprite = nullptr;
     cocos2d::ccColor4B m_color{};
 };
